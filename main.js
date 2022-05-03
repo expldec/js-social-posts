cf7ad0e766b0c466acbb577fb3700dd8c0b86f35
expldec <explosive.deconstruction@gmail.com>
2022-05-03 13:19:19 +0200

    Scaffolding, assets

---
+++ main.js
@@ -0,0 +1,58 @@
+const posts = [
+    {
+        "id": 1,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/600/300?image=171",
+        "author": {
+            "name": "Phil Mangione",
+            "image": "https://unsplash.it/300/300?image=15"
+        },
+        "likes": 80,
+        "created": "2021-06-25"
+    },
+    {
+        "id": 2,
+        "content": "Repellat porro ut fugiat repudiandae? Ea debitis cumque, quibusdam dolores voluptate mollitia.",
+        "media": "https://unsplash.it/600/300?image=172",
+        "author": {
+            "name": "Johnny Ubriacone",
+            "image": "https://unsplash.it/300/300?image=16"
+        },
+        "likes": 48,
+        "created": "2021-06-24"
+    },
+    {
+        "id": 3,
+        "content": "Cum neque, unde reiciendis exercitationem rem officiis architecto ipsum explicabo quasi dignissimos?",
+        "media": "https://unsplash.it/600/300?image=173",
+        "author": {
+            "name": "Nick Ficcanaso",
+            "image": "https://unsplash.it/300/300?image=17"
+        },
+        "likes": 12,
+        "created": "2021-06-23"
+    },
+    {
+        "id": 4,
+        "content": "Impedit ipsam rerum iste aut provident eaque itaque eligendi corporis maxime ullam alias, molestias fuga laboriosam laborum iure expedita ducimus accusantium doloremque?",
+        "media": "https://unsplash.it/600/300?image=174",
+        "author": {
+            "name": "Jack Buffone",
+            "image": "https://unsplash.it/300/300?image=18"
+        },
+        "likes": 0,
+        "created": "2021-06-22"
+    },
+    {
+        "id": 5,
+        "content": "Doloremque voluptatum et maxime aliquid quibusdam natus libero, delectus beatae minus ratione sed vero iste alias, voluptatem soluta exercitationem? Fugit, ad ex?",
+        "media": "https://unsplash.it/600/300?image=175",
+        "author": {
+            "name": "Mary Svampita",
+            "image": "https://unsplash.it/300/300?image=19"
+        },
+        "likes": 150,
+        "created": "2021-06-21"
+    },
+    
+];
